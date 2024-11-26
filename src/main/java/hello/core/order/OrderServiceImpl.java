6099c6c200ba9b1de0573e593971abd75d8a0849
MoseT1 <kimmose981214@gmail.com>
2024-11-26 00:06:16 -0800

    test : spring basic

---
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -0,0 +1,35 @@
+package hello.core.order;
+
+import hello.core.discount.DiscountPolicy;
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
+import hello.core.member.Member;
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+
+public class OrderServiceImpl implements OrderService {
+
+    private final MemberRepository memberRepository;
+    //private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    //private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
+    /*
+    * OrderServiceImpl이 DiscountPolicy에만 의존하는 게 아닌 그 구현체에도 의존함
+    * 그로 인해 구현체가 바뀔 때, 코드를 수정하게 됨 -> DIP 위반
+    * */
+    private DiscountPolicy discountPolicy;
+
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
+
+    @Override
+    public Order createOrder(Long memberId, String itemName, int itemPrice) {
+        Member member = memberRepository.findById(memberId);
+        int discountPrice = discountPolicy.discount(member, itemPrice);
+
+
+        return new Order(memberId, itemName, itemPrice, discountPrice);
+    }
+
+}
