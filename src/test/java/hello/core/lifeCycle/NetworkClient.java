7b52c42b5ccbca618648cc85d10e6eadaf605292
MoseT1 <kimmose981214@gmail.com>
2024-11-26 20:41:10 -0800

    Test : IntializingBean, DisposableBean

---
+++ src/test/java/hello/core/lifeCycle/NetworkClient.java
@@ -0,0 +1,47 @@
+package hello.core.lifeCycle;
+
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+public class NetworkClient implements InitializingBean, DisposableBean {
+
+    private String url; // 접속해야 할 서버의 url
+
+    public NetworkClient() {
+        System.out.println("생성자 호출, url = " + url);
+//        connect();
+//        call("초기화 연결 메시지");
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    // 서비스 시작시 호출
+    public void connect() {
+        System.out.println("connect: " + url);
+    }
+
+    // 연결이 된 상태에서 call을 호출해서 연결한 서버에 메시지를 던질 수 있다고 가정
+    public void call(String message) {
+        System.out.println("call: " + url + " message = " + message);
+    }
+
+    // 서비스 종료시 호출
+    public void disconnect() {
+        System.out.println("close: " + url);
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("NetworkClient.afterPropertiesSet");
+        connect();
+        call("초기화 연결 메시지");
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("NetworkClient.destroy");
+        disconnect();
+    }
+}
